12ddee56e0acb7deced71b9914e591aa592c7bf6
Bruna Sanches <bcpsanches@gmail.com>
2023-08-03 11:47:10 -0300

    manifest e pag 41 (faltando)

--- src/includes/script41.js
+++ src/includes/script41.js
@@ -53,3 +53,117 @@ function verificarRespostas1() {
 
     alert("Você acertou " + corretas2 + " respostas.");
   }
+
+
+  
+  function verificarRespostas3() {
+    var tabela3 = document.getElementById("respostas3");
+    var respostas3 = tabela3.getElementsByClassName("resposta3");
+    var corretas3 = 0;
+    var respostasCorretas3 = [
+      ["1.500,00", "2.000,00", "1.500,00", "1.000,00"]
+    ];
+
+    for (var i = 0; i < respostas3.length; i++) {
+      var resposta3 = respostas3[i].value;
+      var linha3 = Math.floor(i / 4);
+      var coluna3 = i % 4;
+
+      if (resposta3 === respostasCorretas3[linha3][coluna3]) {
+        respostas3[i].style.backgroundColor = "#cfc"; // Marcar como correta
+        corretas3++;
+      } else {
+        respostas3[i].style.backgroundColor = "#f99"; // Marcar como incorreta
+      }
+    }
+
+    alert("Você acertou " + corretas3 + " respostas.");
+  }
+
+
+
+  function verificarRespostas4() {
+    var tabela4 = document.getElementById("respostas4");
+    var respostas4 = tabela4.getElementsByClassName("resposta4");
+    var corretas4 = 0;
+    var respostasCorretas4 = [
+      ["600,00","100,00","03 exame","35,00","20,00","1.085,00"]
+    ];
+
+    for (var i = 0; i < respostas4.length; i++) {
+      var resposta4 = respostas4[i].value;
+      var linha4 = Math.floor(i / 6);
+      var coluna4 = i % 6;
+
+      if (resposta4 === respostasCorretas4[linha4][coluna4]) {
+        respostas4[i].style.backgroundColor = "#cfc"; // Marcar como correta
+        corretas4++;
+      } else {
+        respostas4[i].style.backgroundColor = "#f99"; // Marcar como incorreta
+      }
+    }
+
+    alert("Você acertou " + corretas4 + " respostas.");
+  }
+
+
+  
+  function verificarRespostas5() {
+    var tabela5 = document.getElementById("respostas5");
+    var respostas5 = tabela5.getElementsByClassName("resposta5");
+    var corretas5 = 0;
+    var respostasCorretas5 = [
+      ["água", "depreciação", "aluguel", "alimentação", "água","energia elétrica", "ferramentas", "manutenção do prédio", "combustível do carro", "alimentação","matérias-primas","internet","segurança","contas de água", "energia elétrica","salários", "limpeza","estrutura predial", "estrutura predial","mão de obra", "luz", "internet","material de escritório", "limpeza","telefonia", "material de escritório","salários","telefonia"]
+    ];
+
+    for (var i = 0; i < respostas5.length; i++) {
+      var resposta5 = respostas5[i].value;
+      var linha5 = Math.floor(i / 28);
+      var coluna5 = i % 28;
+
+      if (resposta5 === respostasCorretas5[linha5][coluna5]) {
+        respostas5[i].style.backgroundColor = "#cfc"; // Marcar como correta
+        corretas5++;
+      } else {
+        respostas5[i].style.backgroundColor = "#f99"; // Marcar como incorreta
+      }
+    }
+
+    alert("Você acertou " + corretas5 + " respostas.");
+  }
+
+
+
+
+
+  
+document.getElementById("reiniciar").addEventListener("click", function() {
+  var checkboxes = document.getElementsByName("opcao");
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = false;
+  }
+});
+
+const checkboxes = document.getElementsByName("opcao");
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("click", function() {
+    for (let j = 0; j < checkboxes.length; j++) {
+      if (j !== i) {
+        checkboxes[j].checked = false;
+      }
+    }
+  });
+}
+
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("change", function() {
+    if (checkboxes[0].checked) {
+      alert("Parabéns! Você Acertou!");
+    }
+    else{
+      alert("Resposta Incorreta! Clique no botão REINICIAR e tente novamente!");
+    }
+  });
+}
