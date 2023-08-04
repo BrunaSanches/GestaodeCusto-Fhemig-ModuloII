df2c6e4305294aa903fc4f03f27007a522fc9e63
Bruna Sanches <bcpsanches@gmail.com>
2023-08-04 10:47:17 -0300

    correções 41

--- src/includes/script41.js
+++ src/includes/script41.js
@@ -167,3 +167,34 @@ for (let i = 0; i < checkboxes.length; i++) {
     }
   });
 }
+
+
+function verificarRespostas6() {
+  var tabela6 = document.getElementById("respostas6");
+  var respostas6 = tabela6.getElementsByClassName("resposta6");
+  var corretas6 = 0;
+  var respostasCorretas6 = [
+    ["V", "F", "F","V", "V", "F" , "F","V", "F", "F","V", "V","F","V","V","F", "V","V","V","F"]
+  ];
+
+  var respostasCorretas6_2 = [
+    ["v", "f", "f","v", "v", "f" , "f","v", "f", "f","v", "v","f","v","v","f", "v","v","v","f"]
+  ];
+
+  for (var i = 0; i < respostas6.length; i++) {
+    var resposta6 = respostas6[i].value;
+    var linha6 = Math.floor(i / 20);
+    var coluna6 = i % 20;
+
+    if (resposta6 === respostasCorretas6[linha6][coluna6] || resposta6===respostasCorretas6_2[linha6][coluna6]) {
+      respostas6[i].style.backgroundColor = "#cfc"; // Marcar como correta
+      corretas6++;
+    } else {
+      respostas6[i].style.backgroundColor = "#f99"; // Marcar como incorreta
+    }
+  }
+
+  alert("Você acertou " + corretas6 + " respostas.");
+}
+
+
